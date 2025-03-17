a0313bde4f7867ee4a801c5294ecfd0ff2e8ecb6
mqiou <mqiou9@gmail.com>
2025-03-17 12:09:04 -0400

    Update udacitypractice.sql

--- udacitypractice.sql
+++ udacitypractice.sql
@@ -1,4 +1,4 @@
-BASICS
+------BASICS------
 
 --displays only the top 15 rows (limit function ALWAYS COMES LAST)
        SELECT occurred_at, account_id, channel
@@ -54,15 +54,18 @@ BASICS
        AND (primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%')
        AND (primary_poc NOT LIKE '%eana%');
 
-JOINS
+------JOINS------
 
 -- data lives differently in tables (ie. some tables are updated more regularly, compared with some tables that just capture info like order details)
 -- data normalization is essential - 
        -- arranging data into logical groupings such that each group describes a small part of the whole; 
        --minimizing the amount of duplicate data stored in a database
        --organizing the data such that, when you modify it, you make the change in only one place
+-- INNER JOIN: pulls only the rows for which the ON clause is true (the middle of the Venn diagram)
 
--- inner joins combining info from orders table and company accounts table: shows each order and the associated company name and primary poc 
+-- inner joins combining info from orders table and company accounts table: 
+-- shows each order and the associated company name and primary poc
+
        SELECT orders.standard_qty, 
               orders.gloss_qty, 
               orders.poster_qty, 
@@ -76,21 +79,22 @@ JOINS
 --foreign key (FK): a column in a table that is linked to a primary key of a different table.
 --FK usually shows up multiple times in its own table, but only once (as a PK) in another table.
 
-JOINS PROBLEMS
--- INNER JOIN: pulls only the rows for which the ON clause is true (the middle of the Venn diagram)
-       
+------JOINS PROBLEMS------
        
 --Provide a table for all web_events associated with account name of Walmart. 
 --There should be three columns. Be sure to include the primary_poc, time of the event, and the channel for each event. 
 -- Additionally, you might choose to add a fourth column to assure only Walmart events were chosen.
+              
        SELECT t2.name,t2.primary_poc,t1.occurred_at,t1.channel
        FROM web_events as t1
        JOIN accounts as t2
        ON t1.account_id = t2.id
        WHERE t2.name = 'Walmart';
+
 --Provide a table that provides the region for each sales_rep along with their associated accounts. 
 --Your final table should include three columns: the region name, the sales rep name, and the account name. 
 --Sort the accounts alphabetically (A-Z) according to account name.
+
        SELECT t1.name as rep, t2.name as region, t3.name as company
        FROM sales_reps as t1
        JOIN region as t2
@@ -98,9 +102,11 @@ JOINS PROBLEMS
        JOIN accounts as t3
        ON t3.sales_rep_id = t1.id
        ORDER BY t3.name
+              
 --Provide the name for each region for every order, as well as the account name and the unit price they paid (total_amt_usd/total) for the order.
 --Your final table should have 3 columns: region name, account name, and unit price. 
---A few accounts have 0 for total, so I divided by (total + 0.01) to assure not dividing by zero.       
+--A few accounts have 0 for total, so I divided by (total + 0.01) to assure not dividing by zero.     
+              
        SELECT O.id as order_id, R.name AS region, A.name AS account, O.total_amt_usd/(O.total + 0.01) AS unit_price
        FROM orders as O
        JOIN accounts as A
@@ -110,10 +116,12 @@ JOINS PROBLEMS
        JOIN region as R
        ON R.id = S.region_id
 
-OTHER JOINS 
-----Provide a table that provides the region for each sales_rep along with their associated accounts. 
+-----OTHER JOINS-----
+              
+--Provide a table that provides the region for each sales_rep along with their associated accounts. 
 --This time only for the Midwest region. Your final table should include three columns: 
 --the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) according to account name.
+              
        SELECT S.name Rep, R.name Region, A.name Account
        FROM sales_reps S
        LEFT JOIN region R
@@ -124,6 +132,7 @@ OTHER JOINS
        ORDER BY Account
 
 --Same as above problem, but only show the reps with last name starting with K
+              
        SELECT S.name Rep, R.name Region, A.name Account
        FROM sales_reps S
        LEFT JOIN region R
@@ -138,6 +147,7 @@ OTHER JOINS
 --However, you should only provide the results if the standard order quantity exceeds 100. 
 --Your final table should have 3 columns: region name, account name, and unit price. 
 --In order to avoid a division by zero error, adding .01 to the denominator here is helpful total_amt_usd/(total+0.01).
+
        SELECT R.name as region, A.name as account, O.total_amt_usd/(total+0.01) as unit_price
        FROM orders O
        JOIN accounts A
@@ -164,5 +174,71 @@ OTHER JOINS
        WHERE O.occurred_at BETWEEN '2015-01-01' AND '2016-01-01'
        ORDER BY O.occurred_at
 
+------AGGREGATIONS------
+
+--Which account (by name) placed the earliest order? Your solution should have the account name and the date of the order.
+
+       SELECT A.name, O.occurred_at
+       FROM orders as O
+       JOIN accounts as A
+       ON O.account_id = A.id
+       ORDER BY O.occurred_at 
+       LIMIT 1;
+
+--Find the total sales in usd for each account. 
+--You should include two columns - the total sales for each company's orders in usd and the company name.
+
+       SELECT A.name as company, SUM(O.total_amt_usd) as total_sales
+       FROM accounts A
+       JOIN orders O
+       ON A.id = O.account_id
+       GROUP BY company
+       ORDER BY company;
+
+--Via what channel did the most recent (latest) web_event occur, which account was associated with this web_event? 
+--Your query should return only three values - the date, channel, and account name.
+       SELECT W.occurred_at as date, W.channel as channel, 
+              A.name as account 
+       FROM web_events W
+       JOIN accounts A
+       ON W.account_id = A.id
+       ORDER BY date DESC
+       LIMIT 1;
+
+--Find the total number of times each type of channel from the web_events was used. 
+--Your final table should have two columns - the channel and the number of times the channel was used.    
+
+       SELECT channel, COUNT(id) as num_used
+       FROM web_events
+       GROUP BY channel
+       ORDER BY channel;
+
+--Who was the primary contact associated with the earliest web_event?
+
+       SELECT A.primary_poc 
+       FROM accounts A
+       JOIN web_events W
+       ON A.id = W.account_id 
+       ORDER BY W.occurred_at 
+       LIMIT 1;
+
+--What was the smallest order placed by each account in terms of total usd. Provide only two columns - the account name and the total usd. Order from smallest dollar amounts to largest.
+
+       SELECT A.name as account, MIN(O.total_amt_usd) as smallest_order_amount
+       FROM orders O
+       JOIN accounts A
+       ON A.id = O.account_id
+       GROUP BY A.name
+       ORDER BY smallest_order_amount;
+
+--Find the number of sales reps in each region. Your final table should have two columns - the region and the number of sales_reps. 
+--Order from fewest reps to most reps.
+
+       SELECT R.name as region, COUNT(S.id)as num_reps
+       FROM sales_reps S 
+       JOIN region R
+       ON S.region_id = R.id
+       GROUP BY R.name
+       ORDER BY num_reps
 
 
